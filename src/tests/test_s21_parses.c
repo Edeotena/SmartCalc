4e49045b44948ab9e13a95f04cc17268a9983ceb
edeotena <edeotena13@gmail.com>
2023-03-28 22:41:32 +0300

    Some more tests

--- src/tests/test_s21_parses.c
+++ src/tests/test_s21_parses.c
@@ -15,8 +15,53 @@ START_TEST(parser_1) {
   cp = res;
 
   ck_assert_int_eq(code, SUCCESS);
-  for (int i = 0; i < size; ++i, cp = cp->next) {
-    ck_assert_int_eq(cp->op, tokens[i]);
+  for (int i = 0; i < size; ++i) {
+    Token tk = UNRECOGNIZED;
+    double val = 0;
+    get(&cp, &tk, &val);
+    ck_assert_int_eq(tk, tokens[i]);
+  }
+
+  free_queue(&res);
+}
+END_TEST
+
+START_TEST(parser_2) {
+  queue *res = NULL;
+  char *str = "sin(coc)";
+  int code = parse_to_tokens(str, &res);
+  ck_assert_int_eq(code, FAILURE);
+  ck_assert_ptr_eq(res, NULL);
+}
+END_TEST
+
+START_TEST(parser_3) {
+  int size = 14;
+  const Token tokens[] = {SIN,
+                          OPENING_BRACKET,
+                          COS,
+                          OPENING_BRACKET,
+                          X,
+                          MULTIPLICATION,
+                          SIN,
+                          OPENING_BRACKET,
+                          COS,
+                          OPENING_BRACKET,
+                          X,
+                          CLOSING_BRACKET,
+                          CLOSING_BRACKET,
+                          CLOSING_BRACKET};
+  queue *res = NULL, *cp = NULL;
+  char *str = "sin    (cos   ( x   * sin( cos (  x ))  )";
+  int code = parse_to_tokens(str, &res);
+  cp = res;
+
+  ck_assert_int_eq(code, SUCCESS);
+  for (int i = 0; i < size; ++i) {
+    Token tk = UNRECOGNIZED;
+    double val = 0;
+    get(&cp, &tk, &val);
+    ck_assert_int_eq(tk, tokens[i]);
   }
 
   free_queue(&res);
@@ -28,6 +73,8 @@ Suite *parser_suite() {
   TCase *tcase = tcase_create("parser_suite");
 
   tcase_add_test(tcase, parser_1);
+  tcase_add_test(tcase, parser_2);
+  tcase_add_test(tcase, parser_3);
 
   suite_add_tcase(suite, tcase);
   return suite;

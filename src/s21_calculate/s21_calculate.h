4746b5a2502c287583f99a8d1074c82a4f88e9c8
edeotena <edeotena13@gmail.com>
2023-04-01 11:54:01 +0300

    Removed unused func

--- src/s21_calculate/s21_calculate.h
+++ src/s21_calculate/s21_calculate.h
@@ -6,6 +6,5 @@
 int parse_to_tokens(const char *str_to_parse, queue **res);
 queue *make_rpn(queue *queue_to_rpn, int *code);
 double calc_rpn(queue *func, double x, int *code_error);
-double *get_values(queue *func, double left, double right, int steps);
 
 #endif  // SMARTCALC_S21_CALCULATE_H

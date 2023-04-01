425cbc2eebd7a8017442e54367234b1c91e1c345
edeotena <edeotena13@gmail.com>
2023-04-01 20:53:25 +0300

    Function decomposited

--- src/s21_gtk_gui/s21_mw_callbacks.c
+++ src/s21_gtk_gui/s21_mw_callbacks.c
@@ -90,6 +90,78 @@ void do_calculate(GtkWidget *window, struct widgets_container *data) {
   gtk_label_set_text(GTK_LABEL(data->result), buffer);
 }
 
+int create_points_file(struct widgets_container *data, char *buffer,
+                       double start, double end, int steps) {
+  queue *parsed;
+  int code = parse_to_tokens(
+      (char *)gtk_entry_get_text(GTK_ENTRY(data->calc_field)), &parsed);
+  if (code == SUCCESS) {
+    queue *rpn = make_rpn(parsed, &code);
+    if (code == SUCCESS) {
+      double *values = get_values(rpn, start, end, steps);
+      double step = (end - start) / (steps - 1);
+      double x = start;
+      FILE *function = fopen("function.txt", "w");
+      for (int i = 0; i < steps; ++i) {
+        fprintf(function, "%lf %lf\n", x, values[i]);
+        x += step;
+      }
+      fclose(function);
+      free(values);
+      free_queue(&rpn);
+    } else {
+      sprintf(buffer, "RPN making error\n");
+    }
+    free_queue(&parsed);
+  } else {
+    sprintf(buffer, "Parser error\n");
+  }
+
+  return code;
+}
+
+void create_function_image(struct widgets_container *data, double y_end,
+                           double y_start, double x_end, double x_start) {
+  char x[40], y[40], plot[70];
+  sprintf(x, "set xrange [%s: %s]",
+          gtk_entry_get_text(GTK_ENTRY(data->st_field)),
+          gtk_entry_get_text(GTK_ENTRY(data->end_field)));
+  sprintf(y, "set yrange [%s: %s]",
+          gtk_entry_get_text(GTK_ENTRY(data->yst_field)),
+          gtk_entry_get_text(GTK_ENTRY(data->yend_field)));
+  sprintf(plot, "plot \"function.txt\" title \"scaling: %lf\" ps 0.5",
+          (y_end - y_start) / (x_end - x_start));
+  char *commands_gnu_plot[] = {"set terminal png enhanced truecolor",
+                               "set output \"function.png\"",
+                               "set decimalsign locale",
+                               "set xlabel \"x\"",
+                               "set ylabel \"y\"",
+                               x,
+                               y,
+                               plot,
+                               "set out"};
+  FILE *gnu_plot = popen("gnuplot -persistent", "w");
+  int i;
+
+  for (i = 0; i < 9; i++) {
+    fprintf(gnu_plot, "%s \n", commands_gnu_plot[i]);
+  }
+  pclose(gnu_plot);
+}
+
+void build_image_window() {
+  GtkWidget *pic = gtk_image_new_from_file("function.png");
+  if (pic != NULL) {
+    GtkWidget *func_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    GtkWidget *grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(func_window), grid);
+
+    gtk_grid_attach(GTK_GRID(grid), pic, 0, 0, 1, 2);
+
+    gtk_widget_show_all(func_window);
+  }
+}
+
 void do_build(GtkWidget *window, struct widgets_container *data) {
   if (window == NULL) {
     return;
@@ -97,12 +169,12 @@ void do_build(GtkWidget *window, struct widgets_container *data) {
 
   char buffer[25] = "function built\n";
 
-  double start = 0, end = 0;
-  int valid_start = get_double_from_entry(data->st_field, &start);
-  int valid_end = get_double_from_entry(data->end_field, &end);
-  double ystart = 0, yend = 0;
-  int yvalid_start = get_double_from_entry(data->yst_field, &ystart);
-  int yvalid_end = get_double_from_entry(data->yend_field, &yend);
+  double x_start = 0, x_end = 0;
+  int valid_start = get_double_from_entry(data->st_field, &x_start);
+  int valid_end = get_double_from_entry(data->end_field, &x_end);
+  double y_start = 0, y_end = 0;
+  int yvalid_start = get_double_from_entry(data->yst_field, &y_start);
+  int yvalid_end = get_double_from_entry(data->yend_field, &y_end);
   int steps;
   int valid_steps = get_int_from_entry(data->steps_field, &steps);
 
@@ -110,77 +182,18 @@ void do_build(GtkWidget *window, struct widgets_container *data) {
 
   if (valid_steps == FAILURE || valid_start == FAILURE ||
       yvalid_end == FAILURE || yvalid_start == FAILURE ||
-      valid_end == FAILURE || end <= start || steps < 2) {
+      valid_end == FAILURE || x_end <= x_start || steps < 2) {
     code = FAILURE;
     sprintf(buffer, "Wrong params\n");
   }
 
   if (code == SUCCESS) {
-    queue *parsed;
-    code = parse_to_tokens(
-        (char *)gtk_entry_get_text(GTK_ENTRY(data->calc_field)), &parsed);
-    if (code == SUCCESS) {
-      queue *rpn = make_rpn(parsed, &code);
-      if (code == SUCCESS) {
-        double *values = get_values(rpn, start, end, steps);
-        double step = (end - start) / (steps - 1);
-        double x = start;
-        FILE *function = fopen("function.txt", "w");
-        for (int i = 0; i < steps; ++i) {
-          fprintf(function, "%lf %lf\n", x, values[i]);
-          x += step;
-        }
-        fclose(function);
-        free(values);
-        free_queue(&rpn);
-      } else {
-        sprintf(buffer, "RPN making error\n");
-      }
-      free_queue(&parsed);
-    } else {
-      sprintf(buffer, "Parser error\n");
-    }
+    code = create_points_file(data, buffer, x_start, x_end, steps);
   }
 
   if (code == SUCCESS) {
-    char x[40], y[40], plot[70];
-    sprintf(x, "set xrange [%s: %s]",
-            gtk_entry_get_text(GTK_ENTRY(data->st_field)),
-            gtk_entry_get_text(GTK_ENTRY(data->end_field)));
-    sprintf(y, "set yrange [%s: %s]",
-            gtk_entry_get_text(GTK_ENTRY(data->yst_field)),
-            gtk_entry_get_text(GTK_ENTRY(data->yend_field)));
-    sprintf(plot, "plot \"function.txt\" title \"scaling: %lf\"",
-            (yend - ystart) / (end - start));
-    char *commands_gnu_plot[] = {"set terminal png enhanced truecolor",
-                                 "set output \"function.png\"",
-                                 "set decimalsign locale",
-                                 "set xlabel \"x\"",
-                                 "set ylabel \"y\"",
-                                 x,
-                                 y,
-                                 plot,
-                                 "set out"};
-    FILE *gnu_plot = popen("gnuplot -persistent", "w");
-    int i;
-
-    for (i = 0; i < 9; i++) {
-      fprintf(gnu_plot, "%s \n", commands_gnu_plot[i]);
-    }
-    pclose(gnu_plot);
-  }
-
-  if (code == SUCCESS) {
-    GtkWidget *pic = gtk_image_new_from_file("function.png");
-    if (pic != NULL) {
-      GtkWidget *func_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-      GtkWidget *grid = gtk_grid_new();
-      gtk_container_add(GTK_CONTAINER(func_window), grid);
-
-      gtk_grid_attach(GTK_GRID(grid), pic, 0, 0, 1, 2);
-
-      gtk_widget_show_all(func_window);
-    }
+    create_function_image(data, y_end, y_start, x_end, x_start);
+    build_image_window();
   }
 
   gtk_label_set_text(GTK_LABEL(data->result), buffer);
